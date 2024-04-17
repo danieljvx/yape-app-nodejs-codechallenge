2f018abc90175d38185edd3888f601e7937594dc
Daniel Villanueva <daniel.villanueva@talentpitch.co>
2024-04-16 20:23:20 -0500

    Add docker node-alpine for apps

--- app-transaction/src/main.ts
+++ app-transaction/src/main.ts
@@ -1,7 +1,10 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+  console.log(`app-transaction - ${process.env.PG_HOST || 'localhost'}`);
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
